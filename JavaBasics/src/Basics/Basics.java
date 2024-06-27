27564b2b4f4d9e57d6f3d86069d21cc810109746
GurjinderpalSingh <157075228+GurjinderpalSingh@users.noreply.github.com>
2024-06-26 21:50:47 -0400

    practice code

--- JavaBasics/src/Basics/Basics.java
+++ JavaBasics/src/Basics/Basics.java
@@ -2,6 +2,20 @@ package Basics;
 
 public class Basics {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String firstName = "Gurjinder";
+
+        int num = 686;
+
+        System.out.println(firstName + "\t" + num );
+
+        double num2 =  num * 2;
+
+       System.out.println(Math.max(num, num2));
+
+       // ternary operator 
+       int age = 26;
+       String result = (age>18) ? "can vote" : "Can't vote";
+       System.out.println(result);
+
     }
 }
